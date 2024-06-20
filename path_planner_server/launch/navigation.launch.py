87c57e73c5ee61c6c73dfdd1ba354ec0158d5979
morg1207 <alaurao@uni.pe>
2024-06-20 03:06:04 +0000

    Se agregaron nuevos mapas

--- path_planner_server/launch/navigation.launch.py
+++ path_planner_server/launch/navigation.launch.py
@@ -9,7 +9,8 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     package_path_planner = 'path_planner_server'
     package_localization = 'localization_server'
-    rviz_file = os.path.join(get_package_share_directory(package_path_planner),'rviz','pathplanning.rviz')
+    #rviz_file = os.path.join(get_package_share_directory(package_path_planner),'rviz','pathplanning.rviz')
+    rviz_file = os.path.join(get_package_share_directory('filter_map_laser'),'config','config.rviz')
     
     #~~~~~~~~~~~~~~~~~~~~~~~~path config files~~~~~~~~~~~~~~~~~~~~~~~~~~~
     amcl_file = os.path.join(get_package_share_directory(package_localization),'config','amcl_config.yaml')
@@ -38,6 +39,24 @@ def generate_launch_description():
         ]),
         "'"
     ])
+
+    map_file_edit = LaunchConfiguration('map_file_edit')
+    arg_map_file_edit = DeclareLaunchArgument(
+        'map_file_edit',
+        default_value='warehouse_map_real_edit.yaml',
+        description='Path to the map select'
+    )
+
+    # Obtener la ruta completa del archivo YAML del mapa
+    map_file_path_edit = PythonExpression([
+        "'",
+        PathJoinSubstitution([
+            FindPackageShare(package_localization),
+            'maps',
+            map_file_edit
+        ]),
+        "'"
+    ])
     # Parámetro de argumento de lanzamiento
     bt_xml_file_arg = DeclareLaunchArgument(
         'default_nav_to_pose_bt_xml',
@@ -51,6 +70,7 @@ def generate_launch_description():
     return LaunchDescription([ 
         bt_xml_file_arg,    
         arg_map_file,
+        arg_map_file_edit,
 
         #~~~~~~~~~~~~~~~~~~provide map~~~~~~~~~~~~~~~~~~~~~~~~~~
         Node(
@@ -60,6 +80,17 @@ def generate_launch_description():
             output='screen',
             parameters=[{'use_sim_time': False}, 
                         {'yaml_filename': map_file_path}]
+        ),
+         #~~~~~~~~~~~~~~~~~~map edit~~~~~~~~~~~~~~~~~~~~~~~~~~
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_edit',
+            output='screen',
+            parameters=[{'use_sim_time': False}, 
+                        {'yaml_filename': map_file_path_edit},
+                        {'topic_name' : "/map_edit"}
+                        ]
         ),
 
         #~~~~~~~~~~~~~~~~~~amcl~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -116,6 +147,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{'autostart': True},
                         {'node_names': ['map_server',
+                                        'map_edit',
                                         'amcl',
                                         'planner_server',
                                         'controller_server',
@@ -129,6 +161,6 @@ def generate_launch_description():
             executable='rviz2',
             name='rviz2',
             output='screen',
-            arguments=['-d', rviz_file]
+            arguments=['-d', '/home/user/ros_ws/src/filter_map_laser/config/config.rviz']
         )
     ])
